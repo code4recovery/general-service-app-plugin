b439e7085183d8d0a68d35de930a3134b25f0d99
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2025-10-26 15:46:57 -0700

    initial commit

---
+++ general-service-app.js
@@ -0,0 +1,87 @@
+const element = document.getElementById("general-service-app");
+if (!element) {
+  throw new Error("Element with ID 'general-service-app' not found.");
+}
+
+const types = ["news", "business", "resources"];
+
+const labels = {
+  news: {
+    en: "News",
+    es: "Noticias",
+    fr: "Nouvelles",
+  },
+  business: {
+    en: "Business",
+    es: "Negocios",
+    fr: "Affaires",
+  },
+  resources: {
+    en: "Resources",
+    es: "Recursos",
+    fr: "Ressources",
+  },
+};
+
+const now = new Date();
+
+fetch(`https://generalservice.app/storage/106.json`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // get area stories
+    const stories = data[1].stories.filter(({ start_at, end_at }) => {
+      const startDate = new Date(start_at);
+      const endDate = new Date(end_at);
+      return now >= startDate && now <= endDate;
+    });
+
+    // determine language
+    const languages = stories.map((story) => story.language);
+    const browserLanguage = navigator.language.substring(0, 2);
+    const language = languages.includes(browserLanguage)
+      ? browserLanguage
+      : "en";
+
+    const filteredStories = stories.filter((s) => s.language === language);
+
+    if (!filteredStories.length) {
+      throw new Error(`Story not found for language: ${language}`);
+    }
+
+    element.innerHTML = types
+      .filter(
+        (type) => filteredStories.filter((s) => s.type === type).length > 0
+      )
+      .map((type) => {
+        return `<h2>${labels[type][language]}</h2>${filteredStories
+          .filter((s) => s.type === type)
+          .map(
+            (story) => `<article><h3>${story.title}</h3><p>${
+              story.description
+            }</p>
+            ${
+              story.buttons.filter((button) => button.type === "link").length >
+              0
+                ? `<ul>${story.buttons
+                    .filter((button) => button.type === "link")
+                    .map(
+                      (button) =>
+                        `<li><a href="${button.link}" target="_blank" rel="noopener">${button.title}</a></li>`
+                    )
+                    .join("")}</ul>`
+                : ""
+            }
+            </article>`
+          )
+          .join("")}`;
+      })
+      .join("");
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
---
+++ general-service-app.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: General Service App
+ * Description: Integrates with the A.A. General Service App.
+ * Version: 1.0
+ * Author: General Service App
+ * Author URI: https://generalservice.app
+ */
+
+add_shortcode('general_service_app', function ($atts) {
+
+    wp_enqueue_script('general-service-app', plugins_url('general-service-app.js', __FILE__));
+    wp_enqueue_style('general-service-app', plugins_url('general-service-app.css', __FILE__));
+
+    return '<section id="general-service-app"></section>';
+});
