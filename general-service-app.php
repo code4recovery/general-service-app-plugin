1ffd2808e9b383ce71a8747283eafeaf0caa630c
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2026-05-06 08:46:58 -0700

    nested CSS

--- general-service-app.php
+++ general-service-app.php
@@ -4,10 +4,10 @@
  * Description: Integrates with the A.A. General Service App.
  * Author: General Service App
  * Author URI: https://generalservice.app
- * Version: 1.0.6
+ * Version: 1.0.7
  */
 
-$version = '1.0.6';
+$version = '1.0.7';
 
 add_shortcode('general_service_app', function ($atts) use ($version) {
 
